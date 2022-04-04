f0b8d202afde0c2d88f4c25085ffaa2304e74a40
micjun <jung.michael@protonmail.com>
2022-04-04 15:16:17 -0400

    changes

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { PressableProps, Pressable } from 'react-native'
 
 interface IconProps extends PressableProps {
-  text: string
+  text: string // text for the icon
 }
 
 function Icon(props: IconProps) {
---
+++ src/containers/ProfileIconContainer.tsx
@@ -0,0 +1,2 @@
+import React, { useState } from 'react';
+import { View, ViewProps } from 'react-native';
