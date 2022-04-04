4b24b645c18ce3cb1e102abc0e5cad08dfb61617
micjun <jung.michael@protonmail.com>
2022-04-04 13:25:11 -0400

    Icon

---
+++ src/components/Icon.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { PressableProps, Pressable } from 'react-native'
+
+interface IconProps extends PressableProps {
+  text: string
+}
+
+function Icon(props: IconProps) {
+  const {text} = props
+  return (
+    <Pressable>
+      {text}
+    </Pressable>
+  )
+}
+
+export default Icon
