9bc526723723bb78df1a6ade52ff9e588cc5087c
micjun <jung.michael@protonmail.com>
2022-04-09 17:58:45 -0400

    refactor: ScreenNavgiator story path update

--- storybook/stories/ScreenNavigator/ScreenNavigator.stories.tsx
+++ storybook/stories/ScreenNavigator/ScreenNavigator.stories.tsx
@@ -2,7 +2,7 @@ import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import CenterView from '../CenterView';
 import NavigationMenu from '../../../src/containers/NavigationContainer/NavigationContainer';
-import type { Url } from '../../../src/components/ExternalUrl';
+import type { Url } from '../../../src/components/ExternalUrl/ExternalUrl';
 const urls: Url[] = [
   ['click this', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'],
 ];
