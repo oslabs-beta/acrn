85caceda4d6a5b891500666596a77ab7c65fb7ef
micjun <jung.michael@protonmail.com>
2022-04-09 17:53:39 -0400

    fix: revert back to old button for build

--- src/components/NewButton/NewButton.tsx
+++ src/components/NewButton/NewButton.tsx
@@ -1,36 +1,17 @@
-import { PressableProps, Pressable, Text, StyleSheet } from "react-native";
-import React from "react";
+import { PressableProps, Pressable, Text } from 'react-native';
+import React from 'react';
 
 interface Props extends PressableProps {
-  text?: string
+  text?: string;
 }
-
-const defaultButtonStyle = StyleSheet.create({
-  pressable: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 20,
-    elevation: 4,
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    backgroundColor: 'blue',
-    marginVertical: 5
-  },
-  text: {
-    fontWeight: 'bold',
-    color: 'white',
-    fontSize: 14
-  }
-})
 
 function NewButton(props: Props) {
   const { text } = props;
   return (
-    <Pressable accessibilityRole="button" style = {props.style? props.style.pressable: defaultButtonStyle.pressable} {...props}>
-      <Text style = {props.style? props.style.text: defaultButtonStyle.text }>{text ? text: 'Submit'}</Text>
+    <Pressable accessibilityRole="button" {...props}>
+      <Text>{text}</Text>
     </Pressable>
   );
 }
 
 export default NewButton;
-
