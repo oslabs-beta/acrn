866cc5993300d9eabbc66eea91216e93ff1bddf1
david <github@djs.gg>
2022-04-08 20:05:16 -0400

    resolve lint/tsc errors

--- storybook/stories/NavigationContainer/NavigationContainer.stories.tsx
+++ storybook/stories/NavigationContainer/NavigationContainer.stories.tsx
@@ -1,25 +1,25 @@
-import { storiesOf } from "@storybook/react-native";
-import React from "react";
-import CenterView from "../CenterView";
-import NavigationMenu from "../../../src/containers/NavigationContainer";
-import { Url } from "../../../src/components/ExternalUrl";
-import { Styling } from "../../../src/styles/GeneralViewStyles";
+import { storiesOf } from '@storybook/react-native';
+import React from 'react';
+import CenterView from '../CenterView';
+import NavigationMenu from '../../../src/containers/NavigationContainer';
+import { Url } from '../../../src/components/ExternalUrl';
+import { Styling } from '../../../src/styles/GeneralViewStyles';
 
 const urls: Url[] = [
-  ["React Native Docs", "https://reactnative.dev/"],
+  ['React Native Docs', 'https://reactnative.dev/'],
   [
-    "React Native Components",
-    "https://reactnative.dev/docs/components-and-apis",
+    'React Native Components',
+    'https://reactnative.dev/docs/components-and-apis',
   ],
   [
-    "React Native Accessibility Guide",
-    "https://reactnative.dev/docs/accessibility",
+    'React Native Accessibility Guide',
+    'https://reactnative.dev/docs/accessibility',
   ],
-  ["React Native APIs", "https://reactnative.dev/docs/accessibilityinfo"],
+  ['React Native APIs', 'https://reactnative.dev/docs/accessibilityinfo'],
 ];
-storiesOf("NavigationContainer", module)
+storiesOf('NavigationContainer', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add("Navigation Container", () => (
+  .add('Navigation Container', () => (
     <Styling backgroundColor="red">
       <NavigationMenu
         externalUrls={urls}
--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,8 +1,8 @@
-import "./Button/Button.stories";
-import "./Welcome/Welcome.stories";
-import "./NewButton/NewButton.stories";
-import "./AccessibleTextInput/AccessibleInput.stories";
-import "./NavigationContainer/NavigationContainer.stories";
-import "./CoreTextInput/CoreTextInput.stories";
-import "./IconProfile/IconProfile.stories";
-import "./CreateAccount/CreateAccount.stories"
+import './Button/Button.stories';
+import './Welcome/Welcome.stories';
+import './NewButton/NewButton.stories';
+import './AccessibleTextInput/AccessibleInput.stories';
+import './NavigationContainer/NavigationContainer.stories';
+import './CoreTextInput/CoreTextInput.stories';
+import './IconProfile/IconProfile.stories';
+import './CreateAccount/CreateAccount.stories';
