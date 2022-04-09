4a49aeec7679e0ca49f4733635084c514af9dde9
david <github@djs.gg>
2022-04-08 20:07:37 -0400

    remove unneeded files

--- src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as LoginContainer } from './containers/LoginContainer';
-export { default as AccessibleTextInput } from './components/AccessibleTextInput';
-export { default as NewButton } from './components/NewButton';
