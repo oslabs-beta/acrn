8382a5708703429ce139354089677d0c3e57fb59
micjun <jung.michael@protonmail.com>
2022-04-11 19:36:12 -0400

    test: working test for button press function

--- src/components/NewButton/__tests__/NewButton.test.tsx
+++ src/components/NewButton/__tests__/NewButton.test.tsx
@@ -2,22 +2,23 @@ import React from 'react';
 import NewButton from '../NewButton';
 import { render, fireEvent, RenderAPI } from '@testing-library/react-native';
 import type { ReactTestInstance } from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 
 describe('Accessible Text Input', () => {
   let button: any;
+  const onPress = jest.fn();
   const props = {
     text: 'New Button',
   };
   beforeEach(() => {
-    button = render(<NewButton {...props} />);
+    button = render(<NewButton {...props} onPress={onPress} />);
+    expect(button).toMatchSnapshot();
   });
   test('renders input text', () => {
     expect(button.getByText('New Button')).toBeTruthy();
   });
-  // test('button is pressable', () => {
-  //   const button = render(<NewButton {...props} />);
-  //   button.getByText
-  //   fireEvent(button,'onPress')
-  //   expect(onClick).toHaveBeenCalledTimes(1);
-  // });
+  test('button is pressable', () => {
+    fireEvent.press(button.getByText('New Button'));
+    expect(onPress).toHaveBeenCalled();
+  });
 });
