a5a426d88a03b37f26df1771dca9d1f14b8cc7cc
micjun <jung.michael@protonmail.com>
2022-04-12 00:59:59 -0400

    chore: test name change

--- src/components/Profile/__tests__/Profile.test.tsx
+++ src/components/Profile/__tests__/Profile.test.tsx
@@ -15,7 +15,7 @@ describe('Profile test', () => {
     profileRoot = profile.root;
     expect(profile.toJSON()).toMatchSnapshot();
   });
-  test('placeholder', () => {
+  test('style testing', () => {
     expect(profileRoot.findByType(Image).props.style).toMatchObject({
       height: 100,
       width: 100,
