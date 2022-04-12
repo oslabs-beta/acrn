99ab042706b05ddf39339d2892c8a3ed35ffa264
micjun <jung.michael@protonmail.com>
2022-04-12 17:07:11 -0400

    docs: documentation for NewButton

--- src/components/NewButton/NewButton.tsx
+++ src/components/NewButton/NewButton.tsx
@@ -2,8 +2,30 @@ import { PressableProps, Pressable, Text } from 'react-native';
 import React from 'react';
 
 interface Props extends PressableProps {
+  /**
+   * text displayed inside button
+   */
   text?: string;
 }
+
+/**
+ * ## Usage
+ * ```js
+ * import React from 'react';
+ * import { PressableProps, Pressable, Text } from 'react-native';
+ * import { NewButton } from 'acrn-rn';
+ *
+ * const [credentialsAreSubmittable, setCredentialsAreSubmittable] = useState(false);
+ *
+ * const MyComponent = () => (
+ *   <NewButton
+ *    text = 'New Button'
+ *    onPressIn={() => console.log('onPressIn')}
+ *    onPress={() => console.log('onPress')}
+ *    onPressOut={() => console.log('onPressOut')}
+ *    onLongPress={() => console.log('onLongPress')}/>
+ * )
+ */
 
 function NewButton(props: Props) {
   const { text } = props;
