6adabef71504e523eb3fe6710bbab2c66e00f280
micjun <jung.michael@protonmail.com>
2022-04-12 22:09:26 -0400

    docs: added documentation NewButton

--- src/components/NewButton/NewButton.tsx
+++ src/components/NewButton/NewButton.tsx
@@ -2,8 +2,30 @@ import { PressableProps, Pressable, Text, StyleSheet } from 'react-native';
 import React from 'react';
 
 interface Props extends PressableProps {
+  /**
+   * text displayed inside button
+   */
   text?: string;
 }
+
+/**
+ * ## Usage
+ * ```js
+ * import React from 'react';
+ * import { PressableProps, Pressable, Text } from 'react-native';
+ * import { NewButton } from 'acrn-rn';
+ *
+ * const [credentialsAreSubmittable, setCredentialsAreSubmittable] = useState(false);
+ *
+ * const MyComponent = () => (
+ *   <NewButton
+ *    text = 'New Button'
+ *    onPressIn={() => console.log('onPressIn')}
+ *    onPress={() => console.log('onPress')}
+ *    onPressOut={() => console.log('onPressOut')}
+ *    onLongPress={() => console.log('onLongPress')}/>
+ * )
+ */
 
 const defaultButtonStyle = StyleSheet.create({
   pressable: {
@@ -31,9 +53,7 @@ function NewButton(props: Props) {
       style={defaultButtonStyle.pressable}
       {...props}
     >
-      <Text style = {defaultButtonStyle.text}>
-        {text ? text : 'Submit'}
-      </Text>
+      <Text style={defaultButtonStyle.text}>{text ? text : 'Submit'}</Text>
     </Pressable>
   );
 }
