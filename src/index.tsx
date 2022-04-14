6a6b915d340f80250cfc630090cd215c4193da37
david <github@djs.gg>
2022-04-13 22:42:28 -0400

    complete exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-// TODO Finish exports from src
 export { default as LoginContainer } from './containers/LoginContainer/LoginContainer';
 export { default as AccessibleTextInput } from './components/AccessibleTextInput/AccessibleTextInput';
 export { default as NewButton } from './components/NewButton/NewButton';
